d1f0cce714226221de5e1919b4dc3e8e8f3c3b05
Ludovic Mouline <ludovic.mouline@uni.lu>
2019-04-02 11:27:39 +0200

    add glossary mechanism

--- manuscript/build.sh
+++ manuscript/build.sh
@@ -2,5 +2,6 @@
 
 pdflatex -output-directory build ./main.tex
 bibtex ./build/main
+makeindex  -s "./build/main.ist" -t "./build/main.glg" -o "./build/main.gls" "./build/main.glo"
 pdflatex -output-directory build ./main.tex
 pdflatex -output-directory build ./main.tex
