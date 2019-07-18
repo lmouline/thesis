473c8081a54b7540ae3bac1f7b751a4ea4a50fe3
Ludovic Mouline <ludovic.mouline@uni.lu>
2019-07-18 19:10:40 +0200

    use biber the build.sh

--- manuscript/build.sh
+++ manuscript/build.sh
@@ -4,6 +4,6 @@ pdflatex -output-directory build ./main.tex
 makeindex  -s "./build/main.ist" -t "./build/main.glg" -o "./build/main.gls" "./build/main.glo"
 makeindex -s "./build/main.ist" -t "./build/main.alg" -o "./build/main.acr" "./build/main.acn"
 pdflatex -output-directory build ./main.tex
-bibtex ./build/main
+biber ./build/main
 pdflatex -output-directory build ./main.tex
 pdflatex -output-directory build ./main.tex
